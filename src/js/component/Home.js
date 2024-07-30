dbcd878f2abb17a6eb95ac3dfaa9c3a96f76966b
Antonio <116432648+AntonioBG89@users.noreply.github.com>
2024-07-30 17:19:16 +0000

    Preguntar por lo del color púrpura

--- src/js/component/Home.js
+++ src/js/component/Home.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
@@ -7,47 +7,52 @@ import rigoImage from "../../img/rigo-baby.jpg";
 const Home = () => {
 	const [color, setColor] = useState("");
 	const [active, setActive] = useState(false);
+	const [addColor, setAddColor] = useState(false);
 
-	let changeColor = (color) => {
-		setColor(color);
-	}
-
-	useEffect (() => {
+	useEffect(() => {
 		if (active) {
 			const interval = setInterval(() => {
-				if (color == "red"){
+				if (color == "red") {
 					setColor("yellow")
-				} else if (color == "yellow"){
+				} else if (color == "yellow") {
 					setColor("green")
-				} else if (color == "green"){
+				} else if (color == "green") {
 					setColor("red")
 				}
 			}, 1000)
-			return () => {clearInterval(interval)}
+			return () => { clearInterval(interval) }
 		}
 	}, [active, color])
-	
+
 	return (
-		<main>
-		<div className="text-center">
-			<div className="container mt-5 m-auto bg-black d-flex flex-column" style={{width: "150px", height: "350px", borderRadius: "10px"}}>
-				<div className={`red light ${color == "red" ? "selected" : ""}`} onClick={() => {
-					changeColor("red");
-				}}></div>
-				<div className={`yellow light ${color == "yellow" ? "selected" : ""}`} onClick={() => {
-					changeColor("yellow");
-				}}></div>
-				<div className={`green light ${color == "green" ? "selected" : ""}`} onClick={() => {
-					changeColor("green");
-				}}></div>
+		<div>
+			<div className="text-center">
+				<div className="container mt-5 m-auto bg-black d-flex flex-column" style={{ width: "150px", height: "350px", borderRadius: "10px" }}>
+					<div className={`red light ${color == "red" ? "selected" : ""}`} onClick={() => {
+						setColor("red");
+					}}></div>
+					<div className={`yellow light ${color == "yellow" ? "selected" : ""}`} onClick={() => {
+						setColor("yellow");
+					}}></div>
+					<div className={`green light ${color == "green" ? "selected" : ""}`} onClick={() => {
+						setColor("green");
+					}}></div>
+				</div>
+
 			</div>
-			
+			<p className="container-fluid d-flex justify-content-center mt-5">Para activar primero presiona algún color</p>
+			<div className="container-fluid d-flex justify-content-center mt-1">
+				
+				<button className="btn btn-success" onClick={() => {
+					setActive(!active)
+				}}>Activar</button>
+			</div>
+			<div className="container-fluid d-flex justify-content-center mt-5">
+				<button className="btn btn-info" onClick={() => {
+					setAddColor();
+				}}>Añadir púrpura</button>
+			</div>
 		</div>
-		<div className="container-fluid d-flex justify-content-center mt-5">
-		<button className="btn btn-success" onClick={() => {
-				setActive(!active)}}>Activar</button>
-			</div>
-		</main>
 	);
 }
 
